c21247e0b79b57250907bc5dc342c4600e3fa633
ad1290 <42838203+ad1290@users.noreply.github.com>
2018-09-04 11:11:53 +0300

    Add files via upload

---
+++ ICO/myscript.js
@@ -0,0 +1,10 @@
+
+// var wl_button =document.getElementsByClassName("join_wl")[0]; 
+// var enterEmail = document.getElementsByClassName("input-group")[0]; 
+// var recaptcha = document.getElementsByClassName("input-group")[1];
+// wl_button.addEventListener("click", function(){  
+//  console.log("click"); 
+//  enterEmail.classList.toggle("invisible");
+//  recaptcha.classList.toggle("invisible");
+
+// } );
